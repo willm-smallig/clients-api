c4436698eb4cfb7bd53034919a40271d54ddb8fe
willm smalling <wellintonbg@mail.com>
2026-07-23 11:00:13 +0200

    login completed

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ import clientsRouter from "./routes/clients.routes.js";
 import { connect } from "mongoose";
 import { connectDB } from "./config/db.js";
 import usersRoutes from "./routes/usersRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
 await connectDB();
 app.use("/clients", clientsRouter);
 app.use("/users", usersRoutes);
+app.use("/auth", authRoutes);
 app.listen(3000, () => {
   console.log("Servidor iniciado");
 });
---
+++ controllers/authController.js
@@ -0,0 +1,42 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({
+      email,
+    });
+    if (!user) {
+      return res.status(401).json({
+        message: "Usuario no encontrado",
+      });
+    }
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+      return res.status(401).json({
+        message: "Credenciales incorrectas",
+      });
+    }
+    const token = jwt.sign(
+      {
+        id: user._id,
+        email: user.email,
+        role: user.role,
+      },
+
+      "mundial2026",
+      {
+        expiresIn: "1h",
+      },
+    );
+    res.json({
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
